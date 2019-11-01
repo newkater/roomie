fa427e503b393350112e1bdf8251cafdedab19d6
newkater <kate.korobeinikova@gmail.com>
2019-11-01 12:07:24 +0600

    trying to change register-form. controversial results

--- src/components/register-form/register-form.js
+++ src/components/register-form/register-form.js
@@ -12,21 +12,58 @@ export default class RegisterForm extends Component {
             return (
                 <form>
                     <Field>
-                        <Label isSize="medium">Электронная почта</Label>
-                        <Control>
-                            <Input type="email" placeholder='example@gmail.com' isSize="medium"/>
+                        <Label isSize="medium">В каком городе вы ищете жилье?</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                {COUNTRIES.map(country => <option value={country}>{country}</option>)}
+                            </Select>
                         </Control>
                     </Field>
                     <Field>
-                        <Label isSize="medium">Пароль</Label>
+                        <Label isSize="medium">Максимальное число сожителей</Label>
                         <Control>
-                            <Input type="password" isSize="medium" placeholder='password'/>
+                            <Input type="text" placeholder='' isSize="medium"/>
                         </Control>
                     </Field>
                     <Field>
-                        <Label isSize="medium">Повторите пароль</Label>
+                        <Label isSize="medium">Период аренды</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                <option>1 месяц</option>
+                                <option>3 месяца</option>
+                                <option>6 месяцев</option>
+                                <option>9 месяцев</option>
+                                <option>1 год</option>
+                            </Select>
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label isSize="medium">Языки!!!!!!!!!</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                <option>1 месяц</option>
+                                <option>3 месяца</option>
+                                <option>6 месяцев</option>
+                                <option>9 месяцев</option>
+                                <option>1 год</option>
+                            </Select>
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label isSize="medium">Есть ли вредные привычки</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                <option>Нет</option>
+                                <option>Курение</option>
+                                <option>Алкоголь</option>
+                                <option>Я доверяю людям ;(</option>
+                            </Select>
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label isSize="medium">Дополнительная информация</Label>
                         <Control>
-                            <Input type="password" isSize="medium" placeholder='password'/>
+                            <Input type="text" placeholder='' isSize="medium"/>
                         </Control>
                     </Field>
                 </form>
@@ -60,15 +97,33 @@ export default class RegisterForm extends Component {
                         </Control>
                     </Field>
                     <Field>
-                        <Label isSize="medium">Пароль</Label>
-                        <Control>
-                            <Input type="password" isSize="medium" placeholder='password'/>
+                        <Label isSize="medium">Родной город</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                {COUNTRIES.map(country => <option value={country}>{country}</option>)}
+                            </Select>
                         </Control>
                     </Field>
                     <Field>
-                        <Label isSize="medium">Повторите пароль</Label>
+                        <Label isSize="medium">Университет или колледж</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                {COUNTRIES.map(country => <option value={country}>{country}</option>)}
+                            </Select>
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label isSize="medium">Специальность</Label>
+                        <Control className="is-expanded">
+                            <Select isSize="medium" className="is-fullwidth">
+                                {COUNTRIES.map(country => <option value={country}>{country}</option>)}
+                            </Select>
+                        </Control>
+                    </Field>
+                    <Field>
+                        <Label isSize="medium">Контактный телефон</Label>
                         <Control>
-                            <Input type="password" isSize="medium" placeholder='password'/>
+                            <Input type="tel" isSize="medium" placeholder=''/>
                         </Control>
                     </Field>
                 </form>
--- src/pages/register.js
+++ src/pages/register.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import RegisterForm from "../components/register-form";
 import {Button, Container, Section, Title} from "bloomer";
 
+const registerTitle = {
+    "font-family": "Roboto",
+    "font-style": "normal",
+    "font-weight": "500",
+    "font-size": "48px",
+    "line-height": "56px",
+    "text-align": "center",
+    "letter-spacing": "0.25px",
+
+    "color": "#000000"
+}
 
 class RegisterPage extends Component {
     state = { page: 1}
@@ -18,9 +29,9 @@ class RegisterPage extends Component {
         return (
             <Container>
                 <Section style={{"margin-top": "50px"}}>
-                    { page === 1 && <Title isSize={2} hasTextAlign="centered">Регистрация</Title> }
-                    { page === 2 && <Title isSize={2} hasTextAlign="centered">Завершение регистрации</Title> }
-                    { page === 3 && <Title isSize={2} hasTextAlign="centered">Еще немного вопросов :)</Title> }
+                    { page === 1 && <Title style={registerTitle} hasTextAlign="centered">Регистрация</Title> }
+                    { page === 2 && <Title style={registerTitle} hasTextAlign="centered">Завершение регистрации</Title> }
+                    { page === 3 && <Title style={registerTitle} hasTextAlign="centered">Еще немного вопросов :)</Title> }
                 </Section>
 
                 <Section style={{"max-width": "500px", "margin": "0 auto"}}>
