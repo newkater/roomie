74f0de100ca0523a11f3650ec4cbedf44f21c488
newkater <kate.korobeinikova@gmail.com>
2019-12-23 11:08:18 +0600

    not that ugly polls

--- src/components/poll/poll.js
+++ src/components/poll/poll.js
@@ -9,15 +9,17 @@ export default class Poll extends Component {
         let {id, type, person, positive, negative} = form;
         console.log("Poll ", form);
         return(
-            <div>
+            <div className="single-poll">
                 <div className="poll-title" hidden={(type==="DELETE")}>Добавить пользователя {person} в группу?</div>
                 <div className="poll-title" hidden={(type==="ADD")}>Удалить пользователя {person} из группы?</div>
                 <progress className="progress poll-progress is-link" value={positive} max={positive+negative}/>
-                <div>
-                    <div>Проголосовало {positive+negative}</div>
-                    <Button>1</Button>
-                    <Button>2</Button>
-                </div>
+                <span>
+                    <div className="voted-people">Проголосовало {positive+negative}</div>
+                    <div className="flex-container">
+                        <Button className="flex-item vote-button">Я за!</Button>
+                        <Button className="flex-item vote-button" isColor="#C4C4C4" isOutlined>Я против</Button>
+                    </div>
+                </span>
             </div>
         );
     }
